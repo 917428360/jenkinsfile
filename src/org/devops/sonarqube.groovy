eda036578e20f4c1742f6615864bcc50d809b572
Tiger <917428360@qq.com>
2020-04-20 22:26:34 +0800

    Update sonarqube.groovy

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -1,24 +1,25 @@
 package org.devops
 
+
 //scan
-def SonarScan(projectName,projectDesc,projectPath){
+def SonarScan(sonarServer,projectName,projectDesc,projectPath,branchName){
+    
+    //定义sonar服务器列表
+    def servers = ["test":"sonarqube-test","prod":"sonarqube-prod"]
+    
+    //使用sonar服务器
+    withSonarQubeEnv("${servers[sonarServer]}"){
+
         def scannerHome = "/usr/local/sonar-scanner-3.4.0.1729/"
-        def sonarServer = "http://sonar.alstru.com/"
         def sonarDate = sh  returnStdout: true, script: 'date  +%Y%m%d%H%M%S'
         sonarDate = sonarDate - "\n"
-            
+        
         sh """ 
-            ${scannerHome}/bin/sonar-scanner -Dsonar.host.url=${sonarServer} -Dsonar.projectKey=${projectName} \
+            ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
             -Dsonar.projectName=${projectName} -Dsonar.projectVersion=${sonarDate} -Dsonar.ws.timeout=30 \
-            -Dsonar.login=tiger -Dsonar.password=tiger899181 \
-            -Dsonar.projectDescription=${projectDesc} -Dsonar.links.homepage=http://www.alstru.com \
+            -Dsonar.projectDescription=${projectDesc} -Dsonar.links.homepage=http://www.baidu.com \
             -Dsonar.sources=${projectPath} -Dsonar.sourceEncoding=UTF-8 -Dsonar.java.binaries=target/classes \
-            -Dsonar.java.test.binaries=target/test-classes -Dsonar.java.surefire.report=target/surefire-reports 
+            -Dsonar.java.test.binaries=target/test-classes -Dsonar.java.surefire.report=target/surefire-reports  -Dsonar.branch.name=${branchName} -X
         """
-    
-    
-    //def qg = waitForQualityGate()
-    //if (qg.status != 'OK') {
-        //error "Pipeline aborted due to quality gate failure: ${qg.status}"
-    //}
+    }
 }
